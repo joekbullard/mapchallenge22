3d5541f76afea9c71d050b3c4b2453a70011055c
Joe Bullard <joekbullard@gmail.com>
2022-11-06 01:48:33 +0000

    readme and requirements

---
+++ day04/README.md
@@ -0,0 +1,5 @@
+# Day 04 - Green
+
+This map shows an NDVI raster of North Devon. Normalized Difference Vegetation Index quantifies vegetation by measuring the difference between near-infrared (which vegetation strongly reflects) and red light (which vegetation absorbs).
+
+![Day 02 map](https://github.com/joekbullard/mapchallenge22/blob/main/day04/output.png?raw=true)
--- day04/ndvi_plot.py
+++ day04/ndvi_plot.py
@@ -18,7 +18,11 @@ plt.rcParams.update(
 with rasterio.open("northdevon_ndvi_2.tiff") as src:
 
     fig, ax = plt.subplots(figsize=(15, 15))
+    
+    #plt.style.use("dark_background")
+
     show((src), ax=ax, cmap="RdYlGn")
+
     plt.axis("off")
     ax.set_aspect("equal")
     ax.set_title(
@@ -29,11 +33,16 @@ with rasterio.open("northdevon_ndvi_2.tiff") as src:
     ax.add_artist(
         ScaleBar(dx=1, color="white", box_color="black", location="lower left")
     )
-    ax.text(0.15, 0.01, "Contains OS data © Crown copyright 2022\nSentinel-2 (ESA) image courtesy of the Copernicus SciHub",
-        verticalalignment='bottom', horizontalalignment='left',
+    ax.text(
+        0.15,
+        0.01,
+        "Contains OS data © Crown copyright 2022\nSentinel-2 (ESA) image courtesy of the Copernicus SciHub",
+        verticalalignment="bottom",
+        horizontalalignment="left",
         transform=ax.transAxes,
-        color='white', fontsize=8)
+        color="white",
+        fontsize=8,
+    )
 
     plt.savefig("output.png", bbox_inches="tight", pad_inches=0.2)
     plt.show()
-    
